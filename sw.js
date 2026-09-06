2088999d991f2419d76b6d764b6d9a83e254c169
xingxingq99-creator <xingxingq99@gmail.com>
2026-09-06 20:24:57 +0800

    Add service worker for asset caching
    Implement service worker for caching assets and handling fetch requests.

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'shopbook-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request).then((res) => {
+      if (res && res.status === 200) {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+      }
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
